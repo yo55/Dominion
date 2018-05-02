0ceff42eee32e462c6f268d60ea4c1703889e4a8
yo55 <yoann.brochard@laposte.net>
2018-05-02 09:05:37 +0200

    Cartes victoires victoryValue() et play()

--- src/dominion/card/VictoryCard.java
+++ src/dominion/card/VictoryCard.java
@@ -11,4 +11,19 @@ public abstract class VictoryCard extends Card {
 		// TODO Auto-generated constructor stub
 		super(string, i);
 	}
+	
+	@Override
+	/**
+	 * @see dominion.card.Card#victoryValue(dominion.Player)
+	 */
+	public abstract int victoryValue(Player p);
+	
+	
+	@Override
+	/**
+	 * @see dominion.card.Card#play(dominion.Player)
+	 */
+	public void play(Player p) {
+		// On ne joue pas les cartes Victoires
+	}
 }
