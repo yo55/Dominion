4d6596bb9a0116cd7defdb94dc23ca401487a48e
yo55 <yoann.brochard@laposte.net>
2018-05-08 13:31:17 +0200

    Correction Cellar defausse/pioche en une fois

--- src/dominion/card/base/Cellar.java
+++ src/dominion/card/base/Cellar.java
@@ -29,16 +29,24 @@ public class Cellar extends ActionCard {
 		p.incrementActions(1);
 		
 		// defausse de cartes
+		ArrayList<String> cartes = new ArrayList<>();
 		String carte = "0";
+		
+		// choix des cartes à défausser
 		while(!carte.isEmpty()) {
-			
-			// carte à défausser dans la main
 			carte = p.chooseCard("CELLAR: Choisissez une carte à défausser ", p.cardsInHand(), true);
-			
-			// defausse de la carte
-			p.discardCard(carte);
-			
-			// pioche d'une nouvelle carte
+			if(!carte.isEmpty()) {
+				cartes.add(carte);
+			}
+		}
+		
+		// defausse des cartes choisies
+		for(String c : cartes) {
+			p.discardCard(c);
+		}
+		
+		// pioche de nouvelles cartes
+		for(int i=0; i<cartes.size(); i++) {
 			p.addToHand(p.drawCard());
 		}
 	}
