285fb28b507e65baa1a4c2de1818844bebd5faa6
yo55 <yoann.brochard@laposte.net>
2018-05-14 13:50:34 +0200

    Moneylender créé testé

--- src/dominion/card/base/Moneylender.java
+++ src/dominion/card/base/Moneylender.java
@@ -2,6 +2,7 @@ package dominion.card.base;
 import java.util.*;
 import dominion.*;
 import dominion.card.*;
+import dominion.card.common.Copper;
 
 /**
  * Carte Prêteur sur gages (Moneylender)
@@ -10,4 +11,30 @@ import dominion.card.*;
  * Dans ce cas, +3 Pièces.
  */
 public class Moneylender extends ActionCard {
+	
+	/**
+	 * Constructeur cartes Prêteur sur gages
+	 */
+	public Moneylender() {
+		super("Moneylender", 4);
+	}
+
+	@Override
+	/**
+	 * @see dominion.card.Card#play(dominion.Player)
+	 */	
+	public void play(Player p) {
+		boolean hasCopper = false;
+		for(Card c : p.getTreasureCards()) {
+			if (c.getClass().equals(Copper.class)) {
+				// on a une carte cuivre
+				p.trashCard(c);
+				hasCopper = true;
+				break;
+			}
+		}
+		if(hasCopper) {
+			p.incrementMoney(3);
+		}
+	}
 }
