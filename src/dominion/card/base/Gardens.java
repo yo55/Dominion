8311d7fdc2ea567b0f99175ca5fb80cedd48f498
yo55 <yoann.brochard@laposte.net>
2018-05-14 12:19:45 +0200

    Gardens créé testé

--- src/dominion/card/base/Gardens.java
+++ src/dominion/card/base/Gardens.java
@@ -9,4 +9,19 @@ import dominion.card.*;
  * Vaut 1VP pour chaque 10 cartes dans votre deck (arrondi à l'unité inférieure).
  */
 public class Gardens extends VictoryCard {
+
+	/**
+	 * Constructeur cartes Jardins
+	 */
+	public Gardens() {
+		super("Gardens", 4);
+	}
+
+	@Override
+	/**
+	 * @see dominion.card.VictoryCard#victoryValue(dominion.Player)
+	 */
+	public int victoryValue(Player p) {
+		return p.totalCards().size()/10;
+	}
 }
