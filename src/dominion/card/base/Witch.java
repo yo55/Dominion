fa4d959f688cc2200bbbd5d96c273693b9e886a1
yo55 <yoann.brochard@laposte.net>
2018-05-14 19:47:02 +0200

    Witch créé testé

--- src/dominion/card/base/Witch.java
+++ src/dominion/card/base/Witch.java
@@ -10,4 +10,30 @@ import dominion.card.*;
  * Tous vos adversaires recoivent une carte Curse.
  */
 public class Witch extends AttackCard {
+	
+	/**
+	 * Constructeur cartes sorcières
+	 */
+	public Witch() {
+		super("Witch", 5);
+	}
+
+	@Override
+	/**
+	 * @see dominion.card.Card#play(dominion.Player)
+	 */
+	public void play(Player p) {
+		
+		// + 2 cartes
+		for(int i=0; i<2; i++) {
+			p.addToHand(p.drawCard());
+		}
+		
+		// Chaque adversaire prend une carte malédiction
+		for(Player adv : p.otherPlayers()) {
+			adv.gain("Curse");
+		}
+	
+	}
+	
 }
