57f7e5f72bd6712b17040cbd925604d2eb724bdb
yo55 <yoann.brochard@laposte.net>
2018-05-14 12:20:10 +0200

    Festival créé testé

--- src/dominion/card/base/Festival.java
+++ src/dominion/card/base/Festival.java
@@ -11,4 +11,21 @@ import dominion.card.*;
  * +2 Pièces.
  */
 public class Festival extends ActionCard {
+
+	/**
+	 * Constructeur cartes festival
+	 */
+	public Festival() {
+		super("Festival", 5);
+	}
+
+	@Override
+	/**
+	 * @see dominion.card.Card#play(dominion.Player)
+	 */
+	public void play(Player p) {
+		p.incrementActions(2);
+		p.incrementBuys(1);
+		p.incrementMoney(2);
+	}
 }
